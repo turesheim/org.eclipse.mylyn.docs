80f8fc605ce26222ee3fddd70126c7e3fad7fc3d
Torkild U. Resheim <torkildr@gmail.com>
2011-07-08 09:55:08 +0200

    Added mechanism for directly converting wiki markup to EPUB.

---
+++ org.eclipse.mylyn.docs.epub/src/org/eclipse/mylyn/docs/epub/wikitext/MarkupToEPUB.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Torkild U. Resheim.
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Torkild U. Resheim - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.mylyn.docs.epub.wikitext;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+
+import org.eclipse.mylyn.docs.epub.EPUB2;
+import org.eclipse.mylyn.wikitext.core.parser.MarkupParser;
+import org.eclipse.mylyn.wikitext.core.parser.builder.HtmlDocumentBuilder;
+import org.eclipse.mylyn.wikitext.core.parser.markup.MarkupLanguage;
+import org.eclipse.mylyn.wikitext.core.util.XmlStreamWriter;
+
+/**
+ * This type can be used to create an EBUB directly from WikiText markup.
+ * 
+ * @author Torkild U. Resheim
+ * @since 1.0
+ */
+public class MarkupToEPUB {
+
+	private String bookTitle;
+
+	private MarkupLanguage markupLanguage;
+
+	public MarkupLanguage getMarkupLanguage() {
+		return markupLanguage;
+	}
+
+	public void setMarkupLanguage(MarkupLanguage markupLanguage) {
+		this.markupLanguage = markupLanguage;
+	}
+
+	public void parse(String markupContent, File file) throws Exception {
+		if (markupLanguage == null) {
+			throw new IllegalStateException("must set markupLanguage"); //$NON-NLS-1$
+		}
+		// Create a temporary working folder
+		File workingFolder = File.createTempFile("epub_", null);
+		if (workingFolder.delete() && workingFolder.mkdirs()) {
+			File htmlFile = new File(workingFolder.getAbsolutePath()
+					+ File.separator + "markup.html");
+			FileWriter out = new FileWriter(htmlFile);
+
+			HtmlDocumentBuilder builder = new HtmlDocumentBuilder(out) {
+				@Override
+				protected XmlStreamWriter createXmlStreamWriter(Writer out) {
+					return super.createFormattingXmlStreamWriter(out);
+				}
+			};
+			builder.setXhtmlStrict(true);
+			MarkupParser markupParser = new MarkupParser();
+
+			markupParser.setBuilder(builder);
+			markupParser.setMarkupLanguage(markupLanguage);
+			markupParser.parse(markupContent);
+			// Convert the generated HTML to EPUB
+			EPUB2 epub = new EPUB2();
+			epub.setGenerateToc(true);
+			epub.addItem(htmlFile);
+			epub.setFile(file);
+			epub.assemble();
+		}
+		workingFolder.deleteOnExit();
+
+
+	}
+
+	public String getBookTitle() {
+		return bookTitle;
+	}
+
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
+
+}
