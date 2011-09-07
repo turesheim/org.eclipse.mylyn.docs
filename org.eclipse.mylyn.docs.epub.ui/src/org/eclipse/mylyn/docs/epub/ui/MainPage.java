9efed968c7e1c582f0c9c940eade635c32b2f5bd
Torkild U. Resheim <torkildr@gmail.com>
2011-09-07 22:25:26 +0200

    Added copyright header.

--- org.eclipse.mylyn.docs.epub.ui/src/org/eclipse/mylyn/docs/epub/ui/MainPage.java
+++ org.eclipse.mylyn.docs.epub.ui/src/org/eclipse/mylyn/docs/epub/ui/MainPage.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Torkild U. Resheim.
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Torkild U. Resheim - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.mylyn.docs.epub.ui;
 
 import java.util.Set;
@@ -26,6 +36,7 @@ import org.eclipse.wb.swt.ResourceManager;
 
 public class MainPage extends WizardPage {
 
+	@SuppressWarnings("unused")
 	private DataBindingContext m_bindingContext;
 
 	private Text titleText;
